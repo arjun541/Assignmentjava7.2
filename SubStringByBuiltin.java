11caf910e65c820c2d94a7b8a4aa34deb3091b83
arjun541 <bmkr.541@gmail.com>
2017-04-04 07:04:38 +0530

    Update SubStringByBuiltin.java

--- SubStringByBuiltin.java
+++ SubStringByBuiltin.java
@@ -6,11 +6,11 @@ public class SubStringByBuiltin {
 public static void main(String a[])
 { 
 	System.out.println("enter the source string");
-	Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);//input for source string
 	StringBuilder sb=new StringBuilder(sc.next());
 	System.out.println("enter the search string");
-	String s=sc.next();
-	if(sb.indexOf(s)==-1)
+	String s=sc.next();//input for search string
+	if(sb.indexOf(s)==-1)//for finding whether string is found or not
 		System.out.println(s+" is  not found in "+sb.toString());
 	else
 		System.out.println(s+" is   found in "+sb.toString());
